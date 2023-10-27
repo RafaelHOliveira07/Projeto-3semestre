e8c10f18f08aafc9f512047dbaef986f92c07165
Rafael H Oliveira <rafaelhenrique.oli333@gmail.com>
2023-10-27 01:51:58 -0300

    legenda

--- projeto-reciclame/web/javascript/maps.js
+++ projeto-reciclame/web/javascript/maps.js
@@ -375,3 +375,23 @@ function handleLocationError(browserHasGeolocation, infoWindow, pos) {
 }
 
 window.initMap = initMap;
+var iconBase = 'https://maps.google.com/mapfiles/kml/shapes/';
+var icons = {
+  parking: {
+    icon: iconBase + 'parking_lot_maps.png'
+  },
+  library: {
+    icon: iconBase + 'library_maps.png'
+  },
+  info: {
+    icon: iconBase + 'info-i_maps.png'
+  }
+};
+
+function addMarker(feature) {
+  var marker = new google.maps.Marker({
+    position: feature.position,
+    icon: icons[feature.type].icon,
+    map: map
+  });
+}
