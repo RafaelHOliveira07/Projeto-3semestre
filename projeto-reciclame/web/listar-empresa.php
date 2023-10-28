4ba0879013264de3fe32cc9be97277e184e358d4
Rafael H Oliveira <rafaelhenrique.oli333@gmail.com>
2023-10-28 12:13:01 -0300

    css listar empresa

--- projeto-reciclame/web/listar-empresa.php
+++ projeto-reciclame/web/listar-empresa.php
@@ -10,27 +10,36 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <!-- jQuery library -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
+    <link rel="stylesheet" href="style/style-listEmpre.css">
     <title>Empresas Cadastradas</title>
+    
 </head>
 <body>
     <h1>Empresas Cadastradas</h1>
 
     <h3>Listar Alunos</h3>
-    <table border="1">
-        <tr>
-            <th>Código</th>
-            <th>Nome</th>
-            <th>cnpj</th>
-            <th>email</th>
-            <th>cidade</th>
-            <th>estado</th>
-            <th>endereco</th>
-            <th>numero</th>
-            <th>tel</th>
-            <th>cep</th>
+    <section class="table-sec">
+    <table class="table table-bordered " >
+    <thead class="thead-dark">
+
+            <th scope="col">Código</th>
+            <th scope="col">Nome</th>
+            <th scope="col">cnpj</th>
+            <th scope="col">email</th>
+            <th scope="col">cidade</th>
+            <th scope="col">estado</th>
+            <th scope="col">endereco</th>
+            <th scope="col">numero</th>
+            <th scope="col">tel</th>
+            <th scope="col">cep</th>
+            <th scope="col">açôes</th>
         </tr>
         <?php foreach ($lista as $linha):?>
         <tr>
+          
             <td><?php echo $linha['idEmpresa']?></td>
             <td><?php echo $linha['nome']?></td>
             <td><?php echo $linha['cnpj']?></td>
@@ -41,14 +50,14 @@
             <td><?php echo $linha['numero']?></td>
             <td><?php echo $linha['tel']?></td>
             <td><?php echo $linha['cep']?></td>
-            <td>
+            <td class="act">
                 <a href="#">Atualizar</a>
                 <a href="#">Excluir</a>
             </td>
         </tr>
         <?php endforeach ?>
     </table>
-
+</section>
 
 </body>
 </html>
