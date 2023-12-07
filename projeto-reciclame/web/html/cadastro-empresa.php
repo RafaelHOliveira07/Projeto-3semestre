a0ec9e258c3f0fb5e4009bd0d85d0bc38c3db90d
Rafael H Oliveira <rafaelhenrique.oli333@gmail.com>
2023-12-07 13:42:17 -0300

    ok

--- projeto-reciclame/web/html/cadastro-empresa.php
+++ projeto-reciclame/web/html/cadastro-empresa.php
@@ -17,7 +17,7 @@
 
 <body>
 
-    <section class="titulo"><h1>Cadastro Recicla-me</h1></section>
+   
     <main class=" ">
         <h1>Cadastro de Empresas/Parceiros</h1>
         <p>Preencha os formulario abaixo corretamente e se torne um novo parceiro recicla-me</p>
