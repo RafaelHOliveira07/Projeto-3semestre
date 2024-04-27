fdd970cd7de80dbc27916fb7f0b993586db72e1a
Rafael H Oliveira <107508664+RafaelHOliveira07@users.noreply.github.com>
2024-04-27 15:18:48 -0300

    dahs style update

--- projeto-reciclame/web/html/painel2.php
+++ projeto-reciclame/web/html/painel2.php
@@ -23,78 +23,104 @@ require_once '../javascript/web.php'
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   
  
-    <!-- Carregue a API do Google Maps com um retorno de chamada -->
   <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCnw1VEDXoPg6E4-Fk3SUkIPpOcIx5Y-nk&callback=initMap" async defer></script>
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
 
-
-  <link
-  rel="stylesheet"
-  href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css"
-/>
-<link
-  href="https://fonts.googleapis.com/css?family=Montserrat"
-  rel="stylesheet"
-/>
-
-<link
-  rel="stylesheet"
-  href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css"
-/>
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css"
+    integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <link rel="shortcut icon" href="../img/bin-verde.png" type="image/x-icon">
   <link href="../style/stylemap.css" rel="stylesheet">
   <link rel="stylesheet" href="https://unpkg.com/aos@next/dist/aos.css" />
   <link rel="stylesheet" href="../style/style-log.css">
+
   <link rel="stylesheet" href="../style/painel-style.css">
   <link rel="stylesheet" href="../style/style-charts.css">
 
 
+
   <title>Projeto-Reciclame</title>
 </head>
-
 <body>
-  <main class="main-dahs">
+  <main>
   <header>
-    <div class="h1-logo">
-      <img src="../img/Free_Sample_By_Wix__3_-removebg-preview.png" alt="">
-    </div>
-
-    <nav>
-
-      <ul>
-        <li>
-          <a href="">Inico</a>
-        </li>
-        <li>
-          <a href="">Meus Pontos de Coleta</a>
-        </li>
-        <li>
-          <a href="">Novo Ponto de Coleta</a>
-        </li>
-     
-      </ul>
-   <a href="">Sair</a>
-    </nav>
-
-
-
-  </header>
-  <section >
-  <div class="h2"> <span></span>
-        <h2 class="card-title">Meus Pontos de Coleta</h2>
+     <div class="h1-logo">
+        <img src="../img/Free_Sample_By_Wix__3_-removebg-preview.png" alt="">
       </div>
 
-
-  
-    
-   </section>
-   
-    
-   
+    <nav>
      
- 
+      <ul>
+    
+        <li>
+          <a href=""><span class="material-symbols-outlined">
+            home
+            </span>Inicio</a>
+        </li>
+        <li>
+          <a href=""><span class="material-symbols-outlined">
+            delete
+            </span> Meus Pontos de Coleta</a>
+        </li>
+        <li>
+          <a href="">
+            <span class="material-symbols-outlined">
+              library_add
+              </span>
+          </span>Novo Ponto de Coleta</a>
+        </li>
+  
+      </ul>
+
+    </nav>
+  
+    <div class="exit-link">
+    <a href="" class="">Sair<span class="material-symbols-outlined">
+      logout
+      </span></a>
+</div>
+  </header>
+  <main>
+    <section class="meio">
+    <div class="car-text-login">
    
+    <?php
+    if (!empty($lista)) {
+    // Obtém o nome da empresa da primeira linha (assumindo que o nome é o mesmo para todas as lixeiras)
+    $nomeDaEmpresa = $lista[0]['nome_empresa'];
+
+    // Loop foreach para exibir as lixeiras
+ 
+    $nomeDaEmpresaExibido = false;
+    
+    foreach ($lista as $linha) {
+        if (!$nomeDaEmpresaExibido) {
+            echo "<h3 class='fancy-p'>$nomeDaEmpresa</h3>";
+            $nomeDaEmpresaExibido = true;
+        }
+    
+        // Aqui você também pode exibir outras informações da lixeira, se necessário
+    }
+    
+} else {
+    echo "NOME NÃO DISPONÍVEL!";
+}
+?> <h1 class="fancy">Meus Pontos de Coleta</h1>
+</div> <div class="msg-box">
+<span class="material-symbols-outlined">
+
+  notifications
+  </span>
+
+    
+  
+</div>
+
    <section class="dash-area">
+    
+  <div class="h2"> <span></span>
+    <h2 class="card-title"></h2>
+  </div>
     
        <div id="wrapper">
         
@@ -163,14 +189,7 @@ require_once '../javascript/web.php'
                  </div>
                </div>
    
-               <div class="row">
-                 <div class="float-right edit-on-codepen">
-                   <a
-                     target="_blank"
-                     href="https://codepen.io/apexcharts/pen/pxZKqL"
-                     ><img src="assets/edit-on-codepen.png" alt="Edit on Codepen"
-                   /></a>
-                 </div>
+              
                </div>
              </div>
            </div>
