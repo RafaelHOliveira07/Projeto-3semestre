d4122661a26c3abb4279716c9a840acd795c30cc
Rafael H Oliveira <107508664+RafaelHOliveira07@users.noreply.github.com>
2024-05-19 09:37:14 -0300

    Update painel-final.php

--- projeto-reciclame/web/html/painel-final.php
+++ projeto-reciclame/web/html/painel-final.php
@@ -184,14 +184,14 @@ notifications
     <script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.22.2/moment.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/apexcharts"></script>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-  
+    <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCnw1VEDXoPg6E4-Fk3SUkIPpOcIx5Y-nk&callback=initMap"
+        async defer></script>
 
 
 
 
  
-    <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCnw1VEDXoPg6E4-Fk3SUkIPpOcIx5Y-nk&callback=initMap"
-        async defer></script>
+  
 
 </body>
 
